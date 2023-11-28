1285e27876d672c9ddefac085f77757f79117c4f
kissiwaa-agyemang <ekagyema010@st.ug.edu.gh>
2023-11-28 08:51:20 +0000

    first commit

---
+++ src/Pages/Requirement/Requirement.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Requirement.css";
+const Requirement = () => {
+  return <div>
+    <h1> REQUIREMENTS</h1>
+    <p>Bachelor of Science in Information Technology
+
+Entry requirements:  
+Core: Credit passes in English, Core Mathematics & Social Studies
+Electives: Credit passes in any three subjects, with at least a B3 in Core Mathematics</p>
+<p>Bachelor of Science in Computer Science is offered in second Year. Students who wish to offer Bachelor of Science in Computer Science must offer some courses from the Mathematical Department and Department of Computer Science</p>
+  </div>;
+};
+
+export default Requirement;
---
+++ src/Pages/Research/Research.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Research.css";
+const Research = () => {
+  return <div>Research</div>;
+};
+
+export default Research;
